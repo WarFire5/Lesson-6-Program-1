0870240633353b73e2b3006355379e9802b33870
Татьяна <@WIN-E362SIULNK8>
2023-02-03 01:32:49 +0300

    Добавьте файлы проекта.

---
+++ Lesson 6 Program 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_Program_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A, B, multiplication, remainder;
+           
+            Console.WriteLine("Задайте число 'A':");
+            A = Console.ReadLine();
+                       
+            Console.WriteLine("Задайте число 'B':");
+            B = Console.ReadLine();
+
+            int a = Convert.ToInt32(A);
+
+            int b = Convert.ToInt32(B);
+
+            int c = a * b;
+            
+            int d = b % a;
+
+            multiplication = "Результат умножения A на B: " + c + ";";
+
+            remainder = "Остаток от деления B на A: " + d + ".";
+
+            Console.WriteLine(multiplication);
+
+            Console.WriteLine(remainder);
+            
+            Console.ReadLine();
+        }
+    }
+}
